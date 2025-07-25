cd46e3c8bb0806255307d1d928ac18caf0d2d6db
Babalola, Isaac (Guest) <isaac.babalola@capgemini.com>
2025-07-25 16:32:40 +0000

    Merged PR 88578: using APPLICATIONINSIGHTS_CONNECTION_STRING for application insights
    # Description
    
    Please include a summary of the change:
    - using APPLICATIONINSIGHTS_CONNECTION_STRING for application insights
    
    ## Type of change
    
    Please tick the relevant option for your PR request. Follow Semantic Version: v`MAJOR.MINOR.PATCH`
    
    - [ ] Bug fix - PATCH (non-breaking change which fixes an issue)
    - [ ] New feature - MINOR (non-breaking change which adds functionality)
    - [ ] Breaking change - MAJOR (fix or feature that would cause existing functionality to not work as expected)
    - [ ] This change requires a documentation update, or other non-code files.
    
    # Checklist:
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] I have added tests that prove my fix is effective or that my feature works
    - [ ] New and existing unit tests pass locally with my changes

--- src/app-insights.ts
+++ src/app-insights.ts
@@ -5,7 +5,7 @@ export default () => {
   const instrumentationKey = config.instrumentationKey;
 
   if (instrumentationKey) {
-    appInsights.setup(instrumentationKey)
+    appInsights.setup()
     .setAutoDependencyCorrelation(true)
     .setAutoCollectRequests(true)
     .setAutoCollectPerformance(true, true)
